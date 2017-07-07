467694e934c036b7aa32c3ba02b2e22996303457
surh <surh@users.noreply.github.com>
2017-07-07 00:10:38 -0700

    crops plate and finds plant

--- inst/scripts/pipeline.r
+++ inst/scripts/pipeline.r
@@ -20,17 +20,26 @@ img <- EBImage::channel(x = EBImage::readImage(img_file),mode = "rgb")
 # res
 # display(res$img)
 
-
 res <- find_three_stickers(img = img)
 res
 
 res <- find_fourth_point(x = res)
 res
 
-
 plot_platecrop(img,res)
 
-# To generate combinations
-rows <- c(10,20,30)
-cols <- c(40,80,120,160)
-expand.grid(rows = rows, cols = cols)
+dir.create("output")
+files <- crop_plate(img,res,prefix="output/example1.")
+
+data("m1_20141021tiny")
+sizes <- wrapper_predictdir_9feat(img_dir = "output/",overlaydir = "overlay",maskdir = "mask",m1 = m1)
+sizes$Normalized.size <- sizes$npixels / res$size
+sizes
+
+mask <- readImage("mask/example1.col2.row1.jpeg")
+display(mask)
+
+# # To generate combinations
+# rows <- c(10,20,30)
+# cols <- c(40,80,120,160)
+# expand.grid(rows = rows, cols = cols)
