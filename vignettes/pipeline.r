98a8888986340ea0fbdd13900f933cec9fcc9f48
surh <surh@users.noreply.github.com>
2017-07-08 18:17:33 -0700

    preparing vignette

---
+++ vignettes/pipeline.r
@@ -0,0 +1,11 @@
+---
+title: "Basic imaging pipeline"
+author: "Sur Herrera Paredes"
+date: "`r Sys.Date()`"
+output: rmarkdown::html_vignette
+vignette: >
+  %\VignetteIndexEntry{Basic imaging pipeline}
+  %\VignetteEngine{knitr::rmarkdown}
+  %\VignetteEncoding{UTF-8}
+---
+
